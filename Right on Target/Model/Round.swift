a08741825250bdb8cb2f64e5571a53ba728757b0
AlexKarpinets <alexanderkarpinets@gmail.com>
2022-02-24 21:17:15 +0300

    LifeCycle App & SceneDelegate rootViewController

---
+++ Right on Target/Model/Round.swift
@@ -0,0 +1,35 @@
+//
+//  Round.swift
+//  Right on Target
+//
+//  Created by Karpinets Alexander on 23.02.2022.
+//
+
+import Foundation
+
+protocol GameRoundProtocol {
+    var score: Int { get }
+    var currentSecretValue: Int { get }
+    
+    func calculateScore(with value: Int)
+}
+
+class Round: GameRoundProtocol {
+    
+    var score: Int = 0
+    var currentSecretValue: Int = 0
+    
+    init(secretValue: Int) {
+        currentSecretValue = secretValue
+    }
+    
+    func calculateScore(with value: Int) {
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+        } else {
+            score += 50
+        }
+    }
+    }
